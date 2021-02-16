0075539bc637ed9c22f8ebfa022b0e7758a35b1d
Lui Villegas <luipalencia@gmail.com>
2021-02-16 11:35:23 -0300

    segundo cambio

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
-};
 
-export default cipher;
+  encode (offset, string) { 
+    let final_code = "", message;
+    for (let i=0; i<string.length;i++){ 
+      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90) {
+        message = (((string.charCodeAt(i)-65)+offset)%26)+65;
+      }
+      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+        message = (((string.charCodeAt(i)-97)+offset)%26)+97;
+      } 
+      final_code+=String.fromCharCode(message);
+    }
+    return final_code;
+  },
+  decode (offset, string) {
+    let final_code = "", message;
+        for (let i=0; i<string.length;i++){ 
+          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+            message = (((string.charCodeAt(i)-65)+offset)%26)+65;
+          }
+          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+            message = (((string.charCodeAt(i)-97)+offset)%26)+97;
+          }
+          final_code+=String.fromCharCode(message);
+        }
+        return final_code;
+  },
+  }
+  
+  export default cipher;
