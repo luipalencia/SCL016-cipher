44c5e308b0fecd1426124d4df3fa7aaaf2ca28ea
Lui Villegas <luipalencia@gmail.com>
2021-02-17 13:42:37 -0300

    cambios finales con tests aprobados

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,37 @@
 const cipher = {
 
-  encode (offset, string) { 
+  encode(offset, string) {
     let final_code = "", message;
-    for (let i=0; i<string.length;i++){ 
-      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90) {
-        message = (((string.charCodeAt(i)-65)+offset)%26)+65;
+    if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
+      throw new TypeError('Error, Error');
+    }
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        message = (((string.charCodeAt(i) - 65) + offset) % 26) + 65;
       }
-      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-        message = (((string.charCodeAt(i)-97)+offset)%26)+97;
-      } 
-      final_code+=String.fromCharCode(message);
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        message = (((string.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      final_code += String.fromCharCode(message);
     }
     return final_code;
   },
-  decode (offset, string) {
+  decode(offset, string) {
     let final_code = "", message;
-        for (let i=0; i<string.length;i++){ 
-          if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-            message = (((string.charCodeAt(i)-65)+offset)%26)+65;
-          }
-          else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-            message = (((string.charCodeAt(i)-97)+offset)%26)+97;
-          }
-          final_code+=String.fromCharCode(message);
-        }
-        return final_code;
+    if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
+      throw new TypeError('Error, Error');
+    }
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        message = (((string.charCodeAt(i) - 65) + offset) % 26) + 65;
+      }
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        message = (((string.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      final_code += String.fromCharCode(message);
+    }
+    return final_code;
   },
-  }
-  
-  export default cipher;
+}
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import cipher from './cipher.js';
 
-let buttonToEncode = document.getElementById('buttonToEncode'); 
-    buttonToEncode.addEventListener("click", function () { 
+let buttonToEncode = document.getElementById('buttonToEncode');
+buttonToEncode.addEventListener("click", function () {
     let codeEncode = document.getElementById("main_message");
     let codeToEncode = codeEncode.value;
     let displaceEncode = document.getElementById("displace");
     let displaceToEncode = parseInt(displaceEncode.value);
 
-document.getElementById("final_message").value = cipher.encode(displaceToEncode, codeToEncode); 
+    document.getElementById("final_message").value = cipher.encode(displaceToEncode, codeToEncode);
 });
 
-let buttonToDecode = document.getElementById('buttonToDecode'); 
-    buttonToDecode.addEventListener("click", function () {  
-    let codeDecode = document.getElementById("mainMessage");
+let buttonToDecode = document.getElementById('buttonToDecode');
+buttonToDecode.addEventListener("click", function () {
+    let codeDecode = document.getElementById("main_message");
     let codeToDecode = codeDecode.value;
     let displaceDecode = document.getElementById("displace");
     let displaceToDecode = parseInt(displaceDecode.value);
