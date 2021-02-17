f7279676cbfcc6028a78de02b8de8d7eefd54542
Lui Villegas <luipalencia@gmail.com>
2021-02-17 13:44:21 -0300

    modificaciones

--- src/cipher.js
+++ src/cipher.js
@@ -2,15 +2,22 @@ const cipher = {
 
   encode(offset, string) {
     let final_code = "", message;
+    let i = 0;
     if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
       throw new TypeError('Error, Error');
-    }
-    for (let i = 0; i < string.length; i++) {
+    } 
+    for (i = 0; i < string.length; i++) {
       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
         message = (((string.charCodeAt(i) - 65) + offset) % 26) + 65;
       }
       else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
         message = (((string.charCodeAt(i) - 97) + offset) % 26) + 97;
+      }
+      else if (string.charCodeAt(i)===32) {
+        message = 32;
+      }
+      else if (string.charCodeAt(i) >= 33 && string.charCodeAt(i) <= 66) {
+        message = (((string.charCodeAt(i) + 33) - offset) % 33) + 33;
       }
       final_code += String.fromCharCode(message);
     }
@@ -18,15 +25,22 @@ const cipher = {
   },
   decode(offset, string) {
     let final_code = "", message;
+    let i = 0;
     if (offset == null || offset == 0 || offset == [] || string == null || string == 0 || string == []) {
       throw new TypeError('Error, Error');
     }
-    for (let i = 0; i < string.length; i++) {
+    for (i = 0; i < string.length; i++) {
       if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-        message = (((string.charCodeAt(i) - 65) + offset) % 26) + 65;
+        message = (((string.charCodeAt(i) + 65) - offset) % 26) + 65;
       }
       else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-        message = (((string.charCodeAt(i) - 97) + offset) % 26) + 97;
+        message = (((string.charCodeAt(i) + 97) - (offset * -1)) % 26) + 97;
+      }
+      else if (string.charCodeAt(i)===32) {
+        message = 32;
+      }
+      else if (string.charCodeAt(i) >= 33 && string.charCodeAt(i) <= 66) {
+        message = (((string.charCodeAt(i) + 33) - offset) % 33) + 33;
       }
       final_code += String.fromCharCode(message);
     }
